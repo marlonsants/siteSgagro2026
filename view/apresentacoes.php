439a5029c7bc73c599e4d8a876ffef95283a3069
marlon.santos <marlutcu@gmail.com>
2026-04-15 18:59:34 -0300

    Otimizações de responsividade na tela de apresentações

--- view/apresentacoes.php
+++ view/apresentacoes.php
@@ -3,64 +3,135 @@
 <?php include_once("page_head.php"); ?>
 <body>
 
+<style type="text/css">
+	.presentation-card {
+		margin-bottom: 20px;
+	}
+	.presentation-card iframe {
+		width: 100%;
+		height: 220px;
+		border: none;
+		display: block;
+	}
+	.presentation-card .card-title {
+		padding: 8px 10px;
+		font-size: 13px;
+		text-align: center;
+		background-color: #f5f5f5;
+		border-top: 1px solid #ddd;
+	}
+</style>
+
 <div class="container-fluid">
 	<?php include_once("menu.php"); ?>
 
 	<br><br>
 	<div class="col-md-12">
-			<div class="panel panel-default">
-				<div class="panel-heading" style="text-align: center">
-					<h3>Apresentações dos palestrantes</h3>
-				</div>
-				<div class="panel-body">
-					<div class="row">
-						<div class="col-md-3" style="margin-bottom: 15px">
-						<iframe class="col-md-24" src="../documentos/apresentacoes/XSGAgro_2025_JMGoncalves_TVasconcelos.pdf" width="350" height="300"> </iframe>
-						</div>
-						<div class="col-md-3" style="margin-bottom: 15px">
-						<iframe class="col-md-24" src="../documentos/apresentacoes/TeucleSGAGROXFORUMVfinal.pdf" width="350" height="300"> </iframe>
-						</div>
-						<div class="col-md-3" style="margin-bottom: 15px">
-						<iframe class="col-md-24" src="../documentos/apresentacoes/SGAgroApresentacao.pdf" width="350" height="300"> </iframe>
-						</div>
-						<div class="col-md-3" style="margin-bottom: 15px">
-						<iframe class="col-md-24" src="../documentos/apresentacoes/industria50emagronegocio.pdf" width="350" height="300"> </iframe>
-						</div>
-						
-						<div class="col-md-3" style="margin-bottom: 15px">
-						<iframe class="col-md-24" src="../documentos/apresentacoes/2022_Palestra_UNESP_SGAGRO.pdf" width="350" height="300"> </iframe>
-						</div>
-						<div class="col-md-3" style="margin-bottom: 15px">
-						<iframe class="col-md-24" src="../documentos/apresentacoes/Apresentacao1-FASE2APROVACao~OLAYOUT(V1).pdf" width="350" height="300"> </iframe>
-						</div>
-						<div class="col-md-3" style="margin-bottom: 15px">
-						<iframe class="col-md-24" src="../documentos/apresentacoes/MULHER_RURAL_DESAFIOS_E_OPORTUNIDADES.pdf" width="350" height="300"> </iframe>
-						</div>
-						<div class="col-md-3" style="margin-bottom: 15px">
-						<iframe class="col-md-24" src="../documentos/apresentacoes/PalestraInclusao_Mulher_Versaofinal.pdf" width="350" height="300"> </iframe>
-						</div>
-						<div class="col-md-3" style="margin-bottom: 15px">
-						<iframe class="col-md-24" src="../documentos/apresentacoes/SGAgro.pdf" width="350" height="300"> </iframe>
-						</div>
-						<div class="col-md-3" style="margin-bottom: 15px">
-						<iframe class="col-md-24" src="../documentos/apresentacoes/VIISimposioSGAGROCabanayOrtega.pdf" width="350" height="300"> </iframe>
-						</div>
-						<div class="col-md-3" style="margin-bottom: 15px">
-						<iframe class="col-md-24" src="../documentos/apresentacoes/Bem_estar_psicologico_e_subjetivo_no_Agro_desafios_e_possibilidades.pdf" width="350" height="300"> </iframe>
-						</div>
-						<div class="col-md-3" style="margin-bottom: 15px">
-						<iframe class="col-md-24" src="../documentos/apresentacoes/Guia_Pratico_HRDD_IX_SGAgro.pdf" width="350" height="300"> </iframe>
-						</div>
-						<div class="col-md-3" style="margin-bottom: 15px">
-						<iframe class="col-md-24" src="../documentos/apresentacoes/Bem_estar-palestra_Silvia_Silva_14_de_Junho.pdf" width="350" height="300"> </iframe>
+		<div class="panel panel-default">
+			<div class="panel-heading" style="text-align: center">
+				<h3>Apresentações dos palestrantes</h3>
+			</div>
+			<div class="panel-body">
+				<div class="row">
+
+					<div class="col-xs-12 col-sm-6 col-md-4 presentation-card">
+						<div class="panel panel-default">
+							<iframe src="../documentos/apresentacoes/XSGAgro_2025_JMGoncalves_TVasconcelos.pdf"></iframe>
+							<div class="card-title">X SGAgro 2025 — J. M. Gonçalves &amp; T. Vasconcelos</div>
 						</div>
 					</div>
+
+					<div class="col-xs-12 col-sm-6 col-md-4 presentation-card">
+						<div class="panel panel-default">
+							<iframe src="../documentos/apresentacoes/TeucleSGAGROXFORUMVfinal.pdf"></iframe>
+							<div class="card-title">X SGAgro Forum — Teucle</div>
+						</div>
+					</div>
+
+					<div class="col-xs-12 col-sm-6 col-md-4 presentation-card">
+						<div class="panel panel-default">
+							<iframe src="../documentos/apresentacoes/SGAgroApresentacao.pdf"></iframe>
+							<div class="card-title">SGAgro — Apresentação Geral</div>
+						</div>
+					</div>
+
+					<div class="col-xs-12 col-sm-6 col-md-4 presentation-card">
+						<div class="panel panel-default">
+							<iframe src="../documentos/apresentacoes/industria50emagronegocio.pdf"></iframe>
+							<div class="card-title">Indústria 5.0 no Agronegócio</div>
+						</div>
+					</div>
+
+					<div class="col-xs-12 col-sm-6 col-md-4 presentation-card">
+						<div class="panel panel-default">
+							<iframe src="../documentos/apresentacoes/2022_Palestra_UNESP_SGAGRO.pdf"></iframe>
+							<div class="card-title">Palestra UNESP SGAgro (2022)</div>
+						</div>
+					</div>
+
+					<div class="col-xs-12 col-sm-6 col-md-4 presentation-card">
+						<div class="panel panel-default">
+							<iframe src="../documentos/apresentacoes/Apresentacao1-FASE2APROVACao~OLAYOUT(V1).pdf"></iframe>
+							<div class="card-title">Apresentação — Fase 2 Aprovação</div>
+						</div>
+					</div>
+
+					<div class="col-xs-12 col-sm-6 col-md-4 presentation-card">
+						<div class="panel panel-default">
+							<iframe src="../documentos/apresentacoes/MULHER_RURAL_DESAFIOS_E_OPORTUNIDADES.pdf"></iframe>
+							<div class="card-title">Mulher Rural: Desafios e Oportunidades</div>
+						</div>
+					</div>
+
+					<div class="col-xs-12 col-sm-6 col-md-4 presentation-card">
+						<div class="panel panel-default">
+							<iframe src="../documentos/apresentacoes/PalestraInclusao_Mulher_Versaofinal.pdf"></iframe>
+							<div class="card-title">Palestra: Inclusão da Mulher</div>
+						</div>
+					</div>
+
+					<div class="col-xs-12 col-sm-6 col-md-4 presentation-card">
+						<div class="panel panel-default">
+							<iframe src="../documentos/apresentacoes/SGAgro.pdf"></iframe>
+							<div class="card-title">SGAgro</div>
+						</div>
+					</div>
+
+					<div class="col-xs-12 col-sm-6 col-md-4 presentation-card">
+						<div class="panel panel-default">
+							<iframe src="../documentos/apresentacoes/VIISimposioSGAGROCabanayOrtega.pdf"></iframe>
+							<div class="card-title">VII Simpósio SGAgro — Cabana y Ortega</div>
+						</div>
+					</div>
+
+					<div class="col-xs-12 col-sm-6 col-md-4 presentation-card">
+						<div class="panel panel-default">
+							<iframe src="../documentos/apresentacoes/Bem_estar_psicologico_e_subjetivo_no_Agro_desafios_e_possibilidades.pdf"></iframe>
+							<div class="card-title">Bem-Estar Psicológico e Subjetivo no Agro</div>
+						</div>
+					</div>
+
+					<div class="col-xs-12 col-sm-6 col-md-4 presentation-card">
+						<div class="panel panel-default">
+							<iframe src="../documentos/apresentacoes/Guia_Pratico_HRDD_IX_SGAgro.pdf"></iframe>
+							<div class="card-title">Guia Prático HRDD — IX SGAgro</div>
+						</div>
+					</div>
+
+					<div class="col-xs-12 col-sm-6 col-md-4 presentation-card">
+						<div class="panel panel-default">
+							<iframe src="../documentos/apresentacoes/Bem_estar-palestra_Silvia_Silva_14_de_Junho.pdf"></iframe>
+							<div class="card-title">Bem-Estar — Palestra Sílvia Silva (14 de Junho)</div>
+						</div>
+					</div>
+
 				</div>
 			</div>
 		</div>
-	
+	</div>
+
 
 	<?php require_once('rodape2019.php'); ?>
-	
+
 </div>
 </body>
